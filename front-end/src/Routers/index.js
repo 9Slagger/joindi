5cb7a364745795d8caaa6db7794be1e50aab446f
Bee Nanthiya <nanthiya.ch@gmail.com>
2020-01-14 17:32:23 +0700

    [Add]: Add Manage Tag Page

--- front-end/src/Routers/index.js
+++ front-end/src/Routers/index.js
@@ -3,7 +3,8 @@ import { Router, Switch, Route } from "react-router-dom";
 import { createBrowserHistory as createHistory } from "history";
 import { connect } from "react-redux";
 import HomePage from "../pages/Home";
-import AdminPage from "../pages/Admin/Payment/waiting"
+import AdminPage from "../pages/Admin/Payment/waiting";
+import ManageTag from "../pages/Admin/ManageTag/managetag";
 export const history = createHistory();
 
 const Routers = props => {
@@ -12,12 +13,15 @@ const Routers = props => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/admin" component={AdminPage} />
+        <Route path="/admin/managetag" component={ManageTag} />
       </Switch>
     </Router>
   );
 };
 
-const mapStateToProps = ({ Authentication }) => ({ Authentication });
+const mapStateToProps = ({ Authentication }) => ({
+  Authentication
+});
 
 const mapDispatchToProps = null;
 
---
+++ front-end/src/pages/Admin/ManageTag/managetag.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Input, Row, Col } from "antd";
+
+const { Search } = Input;
+
+export default class ManageTag extends Component {
+  render() {
+    return (
+      <Row>
+        <Col> </Col> <Col> </Col>{" "}
+      </Row>
+    );
+  }
+}
