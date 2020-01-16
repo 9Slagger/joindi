6c53822dc335d5df9bcc7d97e5efcdc117dfbd92
9slagger <b5813568@g.sut.ac.th>
2020-01-16 20:16:59 +0700

    [Merge]: branch footer

--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
-import DefaultLayout from "../../common/DefaultLayout";
-import CardEvents from "../../common/CardEvents";
+import React, { Component } from 'react'
+import DefaultLayout from "../../common/DefaultLayout"
+import CardEvents from "../../common/CardEvents"
 
 export default class index extends Component {
   render() {
     return (
       <DefaultLayout>
-        <CardEvents />
+        <h1>this is homepage</h1>
+    <CardEvents/>
       </DefaultLayout>
-    );
+    )
   }
 }
