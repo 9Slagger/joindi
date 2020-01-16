cd29f83a62aea212ed6e4cb38d79718c98c831b0
titiwatsrikaew <titiwat.sr@gmail.com>
2020-01-16 16:33:58 +0700

    update approve-payment n3

--- front-end/src/pages/Admin/ApprovePayment/approvepayment.js
+++ front-end/src/pages/Admin/ApprovePayment/approvepayment.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
-import { Card, Icon, Row, Col, Button, Input } from "antd";
+import { Card, Icon, Row, Col, Button, Input,Modal } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
 import AdminLayout from "../../../common/AdminLayout";
+const { TextArea,Search } = Input;
+const { confirm } = Modal;
 
-const { Search } = Input;
 
 const tabListNoTitle = [
   {
     key: "Waiting",
     tab: (
-      <span>
+      <span className="sub-header-admin">
         <Icon type="question-circle" />
         Waiting
       </span>
@@ -17,7 +20,7 @@ const tabListNoTitle = [
   {
     key: "Approved",
     tab: (
-      <span>
+      <span className="sub-header-admin">
         <Icon type="check-circle" />
         Approve
       </span>
@@ -26,7 +29,7 @@ const tabListNoTitle = [
   {
     key: "Rejected",
     tab: (
-      <span>
+      <span className="sub-header-admin">
         <Icon type="close-circle" />
         Rejected
       </span>
@@ -39,58 +42,96 @@ export default class ApprovePayment extends Component {
   state = {
     key: "tab1",
     noTitleKey: "app",
+    visible: false,
+    id: '',
+    remark: '',
     theData: [
       {
         id:'1',
         name: 'จ่ายค่ากิจกรรมดูหนังนะจ้ะ',
+        remark:'',
         status:'1'
       },
       {
         id:'2',
         name: 'จ่ายเงินค่ากิจกรรมร้องเพลงที่ตู้คาราโอเกะ',
+        remark:'',
         status:'1'
       },
       {
         id:'3',
         name: 'จ่ายเงินค่ากิจกรรมไปจ่ายชาบูชิ โปรโมชั่นมา 4 จ่าย 3',
+        remark:'',
         status:'1'
       },
       {
         id:'4',
         name: 'จ่ายเงินค่ากิจกรรมไปกินไอติมที่ท่าน้ำนนท์',
+        remark:'',
         status:'1'
       },
       {
         id:'5',
         name: 'จ่ายเงินค่ากิจกรรมไปออกกำลังกายเพื่อสุขภาพที่ดี',
+        remark:'',
         status:'1'
       },
       {
         id:'6',
         name: 'จ่ายเงินค่ากิจกรรมวิ่งสู้ฟัดเพื่อลดความอ้วนนะจ้ะ',
+        remark:'',
         status:'1'
       },
-    ]
+    ],
   };
   
   onTabChange = (key, type) => {
-    // console.log(key, type);
     this.setState({ [type]: key });
   };
 
-  handleApprove = (id) => {
+  approvePayment=(id)=>{
     var theData = [...this.state.theData];
     var index = theData.findIndex(obj => obj.id === id);
     theData[index].status = '2';
     this.setState({theData});
   }
 
-  handleDelete = (id) => {
+  handleApprove = (id) => {
+    confirm({
+      title: 'Do you want to Approve Item?',
+      content: 'When clicked the OK button, this dialog will be closed',
+      onOk: ()=> {
+        this.approvePayment(id);
+      },
+      onCancel:()=>{},
+    });
+  }
+
+  handleDelete = (id,remark) => {
     var theData = [...this.state.theData];
     var index = theData.findIndex(obj => obj.id === id);
     theData[index].status = '3';
-    this.setState({theData});
+    theData[index].remark = remark;
+    this.setState({
+      theData,
+      visible: false
+    });
   }
+
+  modalDelete = (id) => {
+    this.setState({
+      id: id
+    })
+    this.showModal()
+  }
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
+  onChangeRemark = (e) => {
+    this.setState({ remark: e.target.value });
+  };
 
   render() {
     const contentListNoTitle = {
@@ -99,16 +140,32 @@ export default class ApprovePayment extends Component {
           // console.log(obj)
           return(
             <div>
-              <Card style={{ width: "200", height: "20" }}>
+               <Card
+                style={{
+                  width: "200",
+                  boxShadow: " 0px 0px 10px -5px rgba(0,0,0,0.75)",
+                  height: "80px"
+                }}
+                className="card-list"
+              >
                 <Row type="flex" justify="space-between">
                   <Col>{obj.name}</Col>
                   <Col>
-                    <Button style={{ border: "none" }} shape="circle" onClick={()=>this.handleApprove(`${obj.id}`)}>
-                      <Icon type="check-circle" style={{ fontSize: "20px" }} />
-                    </Button>
-                    <Button style={{ border: "none" }} shape="circle" onClick={()=>this.handleDelete(`${obj.id}`)}>
-                      <Icon type="close-circle" style={{ fontSize: "20px" }} />
-                    </Button>
+                  <Button
+                    style={{ border: "none", color: "#345586" }}
+                    shape="circle"
+                    onClick={()=>this.handleApprove(`${obj.id}`)}
+                  >
+                    <Icon type="check-circle" style={{ fontSize: "25px" }} />
+                  </Button>
+                  &nbsp;&nbsp;
+                  <Button
+                    style={{ border: "none", color: "#8D021F" }}
+                    shape="circle"
+                    onClick={()=>this.modalDelete(obj.id)}
+                  >
+                    <Icon type="close-circle" style={{ fontSize: "25px" }} />
+                  </Button>
                   </Col>
                 </Row>
               </Card><br/>
@@ -148,6 +205,7 @@ export default class ApprovePayment extends Component {
               <Card style={{ width: "200", height: "20" }}>
                 <Row type="flex" justify="space-between">
                   <Col>{obj.name}</Col>
+                  <Col>{obj.remark}</Col>
                   {/* <Col>
                     <Button style={{ border: "none" }} shape="circle">
                       <Icon type="check-circle" style={{ fontSize: "20px" }} />
@@ -164,26 +222,66 @@ export default class ApprovePayment extends Component {
         
       ),
     };
+    const { visible, value } = this.state;
+    this.showModal = () => {
+      this.setState({
+        visible: true
+      });
+    };
 
-    return <AdminLayout>
-      <Card
-        style={{ width: "100%", textAlign: "center" }}
-        tabList={tabListNoTitle}
-        activeTabKey={this.state.noTitleKey}
-        // tabBarExtraContent={<a href="#">More</a>}
-        onTabChange={key => {
-          this.onTabChange(key, "noTitleKey");
-        }}
-      >
-        <Search
-          placeholder="input search text"
-          onSearch={value => console.log(value)}
-          style={{ width: "70vh" }}
-        />
+    return (
+      <div>
+        <AdminLayout>
         <br />
-        <br />
-        {contentListNoTitle[this.state.noTitleKey]}
-      </Card>
-    </AdminLayout>;
+        <Card
+          style={{
+            width: "100%",
+            textAlign: "center"
+          }}
+          tabList={tabListNoTitle}
+          activeTabKey={this.state.noTitleKey}
+          onTabChange={key => {
+            this.onTabChange(key, "noTitleKey");
+          }}
+        >
+          <Search
+            placeholder="input search text"
+            onSearch={value => console.log(value)}
+            style={{ width: "70vh" }}
+          />
+          <br />
+          <br />
+          {contentListNoTitle[this.state.noTitleKey]}
+        </Card>
+
+        <Modal visible={visible} footer={null} onCancel={this.handleCancel}>
+          <Row>
+            <span className="head-modal-approve-payment">Reject</span>
+            <hr />
+            <br />
+            <TextArea
+              onChange={(e)=>this.onChangeRemark(e)}
+              placeholder="Controlled autosize"
+              autoSize={{ minRows: 3, maxRows: 5 }}
+              style={{
+                border: "none",
+                boxShadow: " 0px 0px 10px -5px rgba(0,0,0,0.75)"
+              }}
+            />
+          </Row>
+          <Row style={{ textAlign: "center" }}>
+            <br />
+            <Button className="btn-cancle" onClick={this.handleCancel}>Cancle</Button>
+            &nbsp;
+            <Button 
+            className="btn-send" 
+            onClick={()=>this.handleDelete(this.state.id,this.state.remark)}
+            >Send</Button>
+          </Row>
+        </Modal>
+      </AdminLayout>
+      </div>
+      
+    );
   }
 }
