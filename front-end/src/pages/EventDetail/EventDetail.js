a21adfabc13a3a0174a75f0225377ef99e0bea50
Bee Nanthiya <nanthiya.ch@gmail.com>
2020-01-16 14:00:23 +0700

    [Added]: Add Event Detail Page

---
+++ front-end/src/pages/EventDetail/EventDetail.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import DefaultLayout from "../../common/DefaultLayout";
+
+export default class EventDetail extends Component {
+  render() {
+    return <DefaultLayout></DefaultLayout>;
+  }
+}
