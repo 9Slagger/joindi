c2676c34c71075218127a10ddd275aa74f3296ed
BlueMagnolia <whiteowlcat@outlook.com>
2020-01-18 18:25:32 +0700

    [Edited]:Event approve with axios

--- back-end/.eslintrc.js
+++ back-end/.eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
-  extends: 'standard',
+  extends: "standard",
   rules: {
-    'no-trailing-spaces': 0,
-    'object-curly-spacing': 0,
-    'no-multi-spaces': 0,
-    'quotes': 0,
-    'semi': 0
+    "no-trailing-spaces": 0,
+    "object-curly-spacing": 0,
+    "no-multi-spaces": 0,
+    quotes: 0,
+    semi: 0,
+    "no-unused-vars": 0,
+    "prefer-const": 0,
+    "camelcase": 0,
+    "no-undef": 0
   }
-}
+};
--- back-end/server.js
+++ back-end/server.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 });
 (async () => {
   try {
-    await sequelize.sync({  });
+    await sequelize.sync({ alter: true });
     await databaseLoader()
     app.listen(PORT, () => {
       console.log(`start server on port = ${PORT}`);
