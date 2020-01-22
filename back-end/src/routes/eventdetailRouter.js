a0a7bfdff0c8f9d18bc75a66abc3c7c4edff0bb9
NonTitiwat <titiwat.sr@gmail.com>
2020-01-22 11:28:39 +0700

    [Mergr]:develop to buyticket

--- back-end/src/routes/eventdetailRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { eventDetail } = require("../controllers/eventDetail");
-
-router.get("/", eventDetail);
-
-module.exports = router;
