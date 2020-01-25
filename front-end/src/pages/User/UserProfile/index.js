a964dc637a6e30382c21481aaf8e4824fe8d15d8
Bee Nanthiya <nanthiya.ch@gmail.com>
2020-01-25 17:58:32 +0700

    [Edited]: Edit User Profile

--- front-end/src/pages/User/UserProfile/index.js
+++ front-end/src/pages/User/UserProfile/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import UserLayout from "../../../common/UserLayout";
 import PersonalProfile from "./components/PersonalProfile";
+import CompanyProfile from "./components/CompanyProfile";
 
 export default class UserProfile extends Component {
   render() {
     return (
       <UserLayout>
         <PersonalProfile />
+        {/* <CompanyProfile /> */}
       </UserLayout>
     );
   }
