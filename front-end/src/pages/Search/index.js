f5a619c64af35ddee5d405b4147d45e463cc41cc
uthumporn naikorn <utp.naikorn@gmail.com>
2020-01-25 12:23:00 +0700

    [Edited]:responsive search events

--- front-end/src/pages/Search/index.js
+++ front-end/src/pages/Search/index.js
@@ -27,7 +27,7 @@ export default class index extends Component {
       const res = await serviceEvent.getEventApprove();
       eventStatusApprove = res.result;
       this.setState({ eventStatusApprove });
-      console.log(eventStatusApprove)
+      console.log(eventStatusApprove);
     } catch (error) {
       console.log(error);
     }
@@ -39,15 +39,15 @@ export default class index extends Component {
 
   render() {
     const { eventStatusApprove } = this.state;
-    console.count("debug")
-    console.log(this.state)
+    console.count("debug");
+    console.log(this.state);
     let keyword;
     if (
       decodeURIComponent(window.location.search.split("keyword=").length > 1)
     ) {
       keyword = decodeURIComponent(window.location.search.split("keyword=")[1]);
     }
-    let eventFilter = eventStatusApprove && eventStatusApprove.events
+    let eventFilter = eventStatusApprove && eventStatusApprove.events;
     if (
       eventStatusApprove &&
       eventStatusApprove.events &&
@@ -77,29 +77,41 @@ export default class index extends Component {
     return (
       <DefaultLayout {...this.props}>
         <Row type="flex" justify="center">
-          <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-            <p className="textSearchResult">Search Results For "{keyword}"</p>
-          </Col>
-
-          <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-            <Row type="flex" justify="end">
-              <Select
-                defaultValue="Sort By"
-                onChange={this.handleChange}
-                className="selectSortBy"
+          <Col span={22}>
+            <Row type="flex" justify="center">
+              <Col
+                xs={22}
+                sm={22}
+                md={22}
+                lg={12}
+                xl={12}
+                className="colTextSearchResult"
               >
-                <Option value="sortbyupcomming">Upcomming</Option>
-              </Select>
-            </Row>
-          </Col>
+                <span className="textSearchResult">
+                  Search Results For "{keyword}"
+                </span>
+              </Col>
 
-          <Divider />
+              <Col xs={22} sm={22} md={22} lg={12} xl={12}>
+                <Row type="flex" justify="center">
+                  <Select
+                    defaultValue="Sort By"
+                    onChange={this.handleChange}
+                    className="selectSortBy"
+                  >
+                    <Option value="sortbyupcomming">Upcomming</Option>
+                  </Select>
+                </Row>
+              </Col>
+            </Row>
+            <Divider />
+          </Col>
         </Row>
 
         <Row type="flex" justify="center">
           {eventFilter &&
             eventFilter.map(event => (
-              <Col xs={24} sm={12} md={12} lg={12} xl={5} key={event.id}>
+              <Col xs={24} sm={12} md={10} lg={8} xl={5} key={event.id} className="colCardEvent">
                 <CardEvents event={event} />
               </Col>
             ))}
