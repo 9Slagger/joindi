d19e518191da2ba19c4edc78a5c25f3838be70e7
uthumporn naikorn <utp.naikorn@gmail.com>
2020-01-26 16:51:19 +0700

    [Edited]: add {...this.props} all user page

--- front-end/src/pages/User/Bookmark/index.js
+++ front-end/src/pages/User/Bookmark/index.js
@@ -5,7 +5,7 @@ import Bookmark from "./components/Bookmark";
 export default class index extends Component {
   render() {
     return (
-      <UserLayout>
+      <UserLayout {...this.props}>
         <Bookmark />
       </UserLayout>
     );
