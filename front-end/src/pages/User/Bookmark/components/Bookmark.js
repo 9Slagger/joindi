fa89213a5ff5d4843841b65b937208b0de7c9939
uthumporn naikorn <utp.naikorn@gmail.com>
2020-01-26 14:26:43 +0700

    [Edited]:edit bookmark page-2

--- front-end/src/pages/User/Bookmark/components/Bookmark.js
+++ front-end/src/pages/User/Bookmark/components/Bookmark.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import "./Bookmark.css"
+import "./Bookmark.css";
+import CardEvents from "../../../../common/CardEvents"
 import { Row, Col, Divider } from "antd";
 
 export default class Bookmark extends Component {
@@ -8,14 +9,28 @@ export default class Bookmark extends Component {
       <Row>
         <Col className="colTextBookmark">
           <Divider orientation="left">
-            <h3 className="textBookMark">Bookmarks</h3>
+            <h3 className="textBookMark">Bookmark</h3>
           </Divider>
         </Col>
 
-        <Col>
-        {/* <CardEvents/> */}
-<p>xcvbnm,.;lkjhgfdsaqwertyuiop</p>
-        
+        <Col xs={24} sm={12} md={10} lg={8} xl={5}>
+          <CardEvents/>
+
+        </Col>
+
+        <Col xs={24} sm={12} md={10} lg={8} xl={5}>
+          <CardEvents/>
+
+        </Col>
+
+        <Col xs={24} sm={12} md={10} lg={8} xl={5}>
+          <CardEvents/>
+
+        </Col>
+
+        <Col xs={24} sm={12} md={10} lg={8} xl={5}>
+          <CardEvents/>
+
         </Col>
       </Row>
     );
