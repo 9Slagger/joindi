a329f853bfd3e26b46dd4534f06bbb26aef8e619
9slagger <b5813568@g.sut.ac.th>
2020-01-29 20:55:51 +0700

    merge develop

--- back-end/src/routes/eventRouter.js
+++ back-end/src/routes/eventRouter.js
@@ -17,7 +17,7 @@ const {
 const { verify, verifyCustomer, verifyAdmin } = require("../_helper/jwt");
 
 router.post("/", verifyCustomer, createEvent);
-// router.get("/categorie", getEventCatagorieList);
+router.get("/categorie", getEventCatagorieList);
 router.get("/categorie/:categorieId", getCategorieAndEvent);
 router.get("/statusapprove", verify, getEventApprove);
 router.get("/admin", verifyAdmin, adminGetEvents);
@@ -26,6 +26,6 @@ router.put("/pendReject", verifyAdmin, rejectEvent);
 router.get("/myevents", verifyCustomer, getMyEvents)
 router.get("/userjoinevent/:eventId", verifyCustomer, getUserJoinEvent)
 router.get("/:eventId", verify, getEventDetail);
-// router.put("/:eventId", verifyAdmin, updateEvent);
+router.put("/:eventId", verifyAdmin, updateEvent);
 
 module.exports = router;
